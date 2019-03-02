a9cd23e09c1076fbe59940a3560a3d88f5ef0d51
Paul Gureghian <paulgureghian@att.net>
2019-03-01 19:56:40 -0800

    second commit

---
+++ importing_Script.py
@@ -0,0 +1,10 @@
+""" Created in Mar 2019 by Paul A. Gureghian """
+
+""" Import 'create_module' as a module """
+
+### Import module 
+import name_Script as ns
+
+ns.myFunction() 
+
+
---
+++ name_Script.py
@@ -0,0 +1,13 @@
+""" Created in Mar 2019 by Paul A. Gureghian. """
+
+""" Create a module in Python. """
+
+def myFunction():
+    print("The value of __name__ is " + __name__) 
+          
+def main():
+    myFunction()
+
+if __name__ == '__main__':
+    main()           
+
