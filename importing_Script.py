c99542ff44973a3bec1c009bdb76cf398565c81a
Paul Gureghian <paulgureghian@att.net>
2019-03-02 16:29:03 -0800

    Fixed typo

--- importing_Script.py
+++ importing_Script.py
@@ -1,6 +1,6 @@
 """ Created in Mar 2019 by Paul A. Gureghian """
 
-""" Import 'create_module' as a module """
+""" Import 'name_Script.py' as a module """
 
 ### Import module 
 import name_Script as ns
